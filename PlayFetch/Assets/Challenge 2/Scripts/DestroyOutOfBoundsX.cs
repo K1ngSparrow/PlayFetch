cee3f85e083f6164804d9fe24404c401bde05021
John <royj@roverkids.org>
2024-05-06 14:20:11 -0400

    done for today

--- PlayFetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ PlayFetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
+    private float topBound = 30;
+    private float lowerBound = -20;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x > topBound)
         {
             Destroy(gameObject);
-        } 
-        // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
-        {
-            Destroy(gameObject);
-        }
+        } else if (transform.position.x < lowerBound)
+                {
+                    Destroy(gameObject);
+                }
 
     }
 }
